f3807cc70e4b972349b305497bbb393bdb11ebcf
852072289 <hshyxs@foxmail.com>
2019-06-17 17:13:44 +0800

    一劳永逸

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
-import './plugins/axios'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
 import store from './store'
 
 Vue.config.productionTip = false
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -11,7 +12,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'home',
   components: {
-    
+    HelloWorld
   }
 }
 </script>
