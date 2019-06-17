e00d44a645250adb285dee74d32f17d6c596cb3c
xiaolg <2935989672@qq.com>
2019-06-17 15:32:30 +0800

    no message

--- web/src/main.js
+++ web/src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import store from './store'
+import Header from './components/Header.vue'
 
 Vue.config.productionTip = false
+
+Vue.component('vheader',Header);
 
 new Vue({
   router,
