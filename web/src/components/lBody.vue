9f4ca4d91bf961133cea7e4376b03795ef596c05
852072289 <hshyxs@foxmail.com>
2019-06-17 20:47:17 +0800

    没啥东西

---
+++ web/src/components/lBody.vue
@@ -0,0 +1,14 @@
+<template>
+		<div>111</div>
+</template>
+
+<script>
+export default {
+		name: 'lbody',
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
